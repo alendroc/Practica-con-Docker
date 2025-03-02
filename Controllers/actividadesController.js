3c26de3f8b63d89f1779e93ef41a14c45ef01ad3
Pipe1844 <pipevifa@gmail.com>
2025-03-01 19:38:56 -0600

    Api actividades completo

--- Controllers/actividadesController.js
+++ Controllers/actividadesController.js
@@ -1,14 +1,48 @@
 const db = require('../database/db');
 
-exports.buscarActividad = (req, res) => {
-  const { id } = req.params;
-  const query = 'SELECT * FROM actividad WHERE id_actividad = ?';
+exports.listarActividades = (req, res) => {
+    const query = 'SELECT * FROM actividad';
 
-  db.query(query, [id], (err, results) => {
-    if (err) {
-      console.error('Error en la consulta:', err);
-      return res.status(500).send('Error en la consulta');
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error en la consulta:', err);
+            return res.status(500).send('Error en la consulta');
+        }
+        res.json(results);
+    });
+};
+
+exports.buscarActividad = (req, res) => {
+    const { id } = req.params;
+    const query = 'SELECT * FROM actividad WHERE id_actividad = ?';
+
+    db.query(query, [id], (err, results) => {
+        if (err) {
+            console.error('Error en la consulta:', err);
+            return res.status(500).send('Error en la consulta');
+        }
+        res.json(results);
+    });
+};
+
+exports.crearActividad = (req, res) => {
+    const { nombre_actividad, cantidad_personas, fecha_actividad, lugar_actividad, cedula_cliente } = req.body;
+
+    if (!nombre_actividad || !cantidad_personas || !fecha_actividad || !lugar_actividad || !cedula_cliente) {
+        return res.status(400).json({ message: 'Todos los campos son requeridos: nombre_actividad, cantidad_personas, fecha_actividad, lugar_actividad y cedula_cliente' });
     }
-    res.json(results);
-  });
+
+    const query = 'INSERT INTO actividad (nombre_actividad, cantidad_personas, fecha_actividad, lugar_actividad, cedula_cliente) VALUES (?, ?, ?, ?, ?)';
+
+    db.query(query, [nombre_actividad, cantidad_personas, fecha_actividad, lugar_actividad, cedula_cliente], (err, results) => {
+        if (err) {
+            console.error('Error al insertar la actividad:', err);
+            return res.status(500).json({ message: 'Error al insertar la actividad' });
+        }
+
+        res.status(201).json({
+            message: 'Actividad creada exitosamente',
+            data: { id: results.insertId, nombre_actividad, cantidad_personas, fecha_actividad, lugar_actividad, cedula_cliente }
+        });
+    });
 };
--- Routes/actividadesRoutes.js
+++ Routes/actividadesRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const actividadesController = require('../Controllers/actividadesController');
 
-router.get('/actividades/:id', actividadesController.buscarActividad());
+router.get('/actividades', actividadesController.listarActividades);
 
-//router.post('/actividades', actividadesController.crearActividad);
+router.get('/actividades/:id', actividadesController.buscarActividad);
+
+router.post('/actividades', actividadesController.crearActividad);
 
 module.exports = router;
